60121c3b859656c8fc4f741ae0488f59574b8b84
Figo2360 <filip.vuletic-antic@fer.unizg.hr>
2026-01-27 17:27:57 +0100

    Dodatna poboljsanja izgleda

--- projectfront/src/components/Appbar.js
+++ projectfront/src/components/Appbar.js
@@ -8,6 +8,9 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import FolderOpenIcon from '@mui/icons-material/FolderOpen';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { ListItemIcon, ListItemText } from '@mui/material';
 
 const Appbar = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -25,6 +28,16 @@ const Appbar = () => {
   const handleNavigate = () => {
     handleClose();
     navigate('/pregled-datoteka');
+  };
+
+  const menuItemStyle = {
+    fontSize: '0.9rem',
+    padding: '10px 20px',
+    transition: 'all 0.2s',
+    '&:hover': {
+      backgroundColor: 'rgba(65, 105, 225, 0.08)',
+      color: 'primary.main',
+    },
   };
 
   return (
@@ -47,16 +60,37 @@ const Appbar = () => {
             open={open}
             onClose={handleClose}
             anchorOrigin={{
-              vertical: 'top',
+              vertical: 'bottom',
               horizontal: 'left',
             }}
             transformOrigin={{
               vertical: 'top',
               horizontal: 'left',
             }}
+            slotProps={{
+              paper: {
+                sx: {
+                  marginTop: '-0.5vh',
+                  borderRadius: '12px',
+                  boxShadow: '0px 4px 20px rgba(0,0,0,0.1)',
+                  minWidth: '180px',
+                }
+              }
+            }}
           >
-            <MenuItem onClick={handleNavigate}>Pregled datoteka</MenuItem>
-            <MenuItem onClick={handleClose}>Moja opcija 2</MenuItem>
+            <MenuItem onClick={handleNavigate} sx={menuItemStyle}>
+            <ListItemIcon>
+              <FolderOpenIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>Pregled datoteka</ListItemText>
+          </MenuItem>
+
+          <MenuItem onClick={handleClose} sx={menuItemStyle}>
+            <ListItemIcon>
+              <SettingsIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>Moja opcija 2</ListItemText>
+          </MenuItem>
           </Menu>
           <Typography variant="h6" component="div" sx={{
               position: 'absolute',
